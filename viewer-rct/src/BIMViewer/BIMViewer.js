2fb68c979652702b741f5f3166b485b77b8eed22
Sergey_Oleynik <tiktaalik@yandex.ru>
2025-04-18 16:48:28 +0300

    ---2

--- viewer-rct/src/BIMViewer/BIMViewer.js
+++ viewer-rct/src/BIMViewer/BIMViewer.js
@@ -1,12 +1,16 @@
-import {BCFViewpointsPlugin, FastNavPlugin, math, stats, Viewer,} from "@xeokit/xeokit-sdk/dist/xeokit-sdk.es.js";
+import {  BCFViewpointsPlugin,
+  FastNavPlugin,
+  math,
+  stats,
+  Viewer,
+} from "@xeokit/xeokit-sdk/dist/xeokit-sdk.es.js";
 import { Controller } from "./forkedComponents/src/Controller";
 import { LocaleService, ObjectsKdTree3 } from "@xeokit/xeokit-sdk";
 import { CanvasContextMenu } from "./forkedComponents/src/contextMenus/CanvasContextMenu";
 import { ObjectContextMenu } from "./forkedComponents/src/contextMenus/ObjectContextMenu";
 import { ModelsContextMenu } from "./forkedComponents/src/contextMenus/ModelsContextMenu";
-import { SectionToolContextMenu } from './forkedComponents/src/contextMenus/SectionToolContextMenu'
-import { TreeViewContextMenu } from './forkedComponents/src/contextMenus/TreeViewContextMenu'
-
+import { SectionToolContextMenu } from "./forkedComponents/src/contextMenus/SectionToolContextMenu";
+import { TreeViewContextMenu } from "./forkedComponents/src/contextMenus/TreeViewContextMenu";
 
 class BIMViewer extends Controller {
   constructor(server, cfg = {}) {
@@ -41,42 +45,41 @@ class BIMViewer extends Controller {
       inspectorElement,
       toolbarElement,
       navCubeCanvasElement,
-      localeService
+      localeService,
     } = cfg;
 
     const viewer = new Viewer({
-        canvasElement,
-        localeService
-    })
+      canvasElement,
+      localeService,
+    });
 
-    super(null, cfg, server, viewer)
+    super(null, cfg, server, viewer);
 
-    this._configs = {}
-    this._enableAddModels = false
-    this._enableMeasurements = true
-    this._enablePropertiesInspector = true
+    this._configs = {};
+    this._enableAddModels = false;
+    this._enableMeasurements = true;
+    this._enablePropertiesInspector = true;
 
-    this.viewer = viewer
+    this.viewer = viewer;
 
-    this._objectKdTree3 = new ObjectsKdTree3(({viewer}))
-    this._customizeViewer()
+    this._objectKdTree3 = new ObjectsKdTree3({ viewer });
+    this._customizeViewer();
 
-    this._explorerElement = explorerElement
-    this._inspectorElement = inspectorElement
+    this._explorerElement = explorerElement;
+    this._inspectorElement = inspectorElement;
 
-    this._modelsExplorer = {}
-    this._objectsExplorer = {}
-    this._classesExplorer = {}
-    this._storeysExplorer = {}
-    this._propertiesInspector = {}
-    
+    this._modelsExplorer = {};
+    this._objectsExplorer = {};
+    this._classesExplorer = {};
+    this._storeysExplorer = {};
+    this._propertiesInspector = {};
   }
 
-
-  get localeService() { return this.viewer.localeService}
+  get localeService() {
+    return this.viewer.localeService;
+  }
 
   _customizeViewer() {
-
     const scene = this.viewer.scene;
 
     // Emphasis effects
@@ -127,10 +130,13 @@ class BIMViewer extends Controller {
     this.viewer.cameraControl.dollyMinSpeed = 0.04;
     this.viewer.cameraControl.dollyProximityThreshold = 30.0;
 
-
     //Доработай CameraPivotElement, Ввынеси из классА, сделай аргументом конструктора и компонентом реакта.
     //Вообще, почему он цепляется к document?
-    const cameraPivotElement = document.createRange().createContextualFragment("<div class='xeokit-camera-pivot-marker'></div>").firstChild;
+    const cameraPivotElement = document
+      .createRange()
+      .createContextualFragment(
+        "<div class='xeokit-camera-pivot-marker'></div>"
+      ).firstChild;
     document.body.appendChild(cameraPivotElement);
     this.viewer.cameraControl.pivotElement = cameraPivotElement;
 
@@ -146,14 +152,43 @@ class BIMViewer extends Controller {
     sao.enabled = true;
     sao.numSamples = 50;
     sao.kernelRadius = 200;
-}
-
-  _initCanvasContextMenus() {
-    this._canvasContextMenu = new CanvasContextMenu(this, {/* как нам материалы генплана  */})
   }
 
+  _initCanvasContextMenus() {
+    this._canvasContextMenu = new CanvasContextMenu(this, {
+      hideOnAction: true,
+      enableMeasurements: true,
+    });
+    this._objectContextMenu = new ObjectContextMenu(this, {
+      hideOnAction: true,
+      enableMeasurements: true,
+    });
 
-
+    const getCanvasPosFromEvent = function (event) {
+      const canvasPos = [];
+      if (!event) {
+        event = window.event;
+        canvasPos[0] = event.x;
+        canvasPos[1] = event.y;
+      } else {
+        let element = event.target;
+        let totalOffsetLeft = 0;
+        let totalOffsetTop = 0;
+        let totalScrollX = 0;
+        let totalScrollY = 0;
+        while (element.offsetParent) {
+          totalOffsetLeft += element.offsetLeft;
+          totalOffsetTop += element.offsetTop;
+          totalScrollX += element.scrollLeft;
+          totalScrollY += element.scrollTop;
+          element = element.offsetParent;
+        }
+        canvasPos[0] = event.pageX + totalScrollX - totalOffsetLeft;
+        canvasPos[1] = event.pageY + totalScrollY - totalOffsetTop;
+      }
+      return canvasPos;
+    };
+  }
 }
 
 export default BIMViewer;
